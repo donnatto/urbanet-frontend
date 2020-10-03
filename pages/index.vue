293d4f81dfee7db5993e6320f319d77d7622b004
Edwin Dominguez <edwindominguez@hotmail.com>
2020-10-03 01:30:30 -0500

    add frontpage images

--- pages/index.vue
+++ pages/index.vue
@@ -11,14 +11,16 @@
   >
     <!-- Text slides with image -->
     <b-carousel-slide
-      img-src="https://picsum.photos/1024/480/?image=52"
+      img-src="https://images.unsplash.com/photo-1558036117-15d82a90b9b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024q=80"
     />
 
     <!-- Slides with custom text -->
-    <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54" />
+    <b-carousel-slide
+      img-src="https://images.unsplash.com/photo-1568634143420-dc5368d74e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&q=80" />
 
     <!-- Slides with image only -->
-    <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58" />
+    <b-carousel-slide
+      img-src="https://images.unsplash.com/photo-1560185127-6ed189bf02f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&q=80" />
 
     <!-- Slides with img slot -->
     <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
